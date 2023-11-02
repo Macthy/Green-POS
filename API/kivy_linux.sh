e6ca59c43bb10d6b98488e4b03dfa22b693074e2
Masha <141708496+Macthy@users.noreply.github.com>
2023-11-02 14:59:35 +0300

    revised kivy_linux.sh

--- API/kivy_linux.sh
+++ API/kivy_linux.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
-echo "[INFO]  Installing Kivy Dependencies..."
-sudo apt install -y \
-    python3-pip \
+# Install Xvfb and other necessary dependencies
+echo "[INFO] Installing Xvfb and other necessary dependencies..."
+sudo apt-get update
+sudo apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
+
+# Start Xvfb
+echo "[INFO] Starting Xvfb..."
+Xvfb :99 -ac &
+export DISPLAY=:99
+
+# Initialize virtual environment and set environment variables
+echo "[INFO] Installing and initializing a virtual environment..."
+python3.5 -m venv kivyenv
+source kivyenv/bin/activate
+mkdir ~/.runtime
+export XDG_RUNTIME_DIR="$HOME/.runtime"
+
+# Install Kivy Dependencies
+echo "[INFO] Installing Kivy Dependencies..."
+sudo apt-get update
+sudo apt-get install -y \
     build-essential \
-    git \
-    python3 \
-    python3-dev \
+    libmtdev1 \
     ffmpeg \
     libsdl2-dev \
+    xorg-dev \
+    xfce4 \
     libsdl2-image-dev \
     libsdl2-mixer-dev \
     libsdl2-ttf-dev \
@@ -17,22 +35,41 @@ sudo apt install -y \
     libavformat-dev \
     libavcodec-dev \
     zlib1g-dev
-echo "[INFO]  Installing Audio Plugins..."
+
+# Install Audio Plugins
 sudo apt install -y \
-    libgstreamer1.0 \
     gstreamer1.0-plugins-base \
     gstreamer1.0-plugins-good
-echo "[DONE]  Dependencies installed..."
 
-pip3 install --upgrade virtualenv setuptools
-#Uncomment following lines to install in a virtual environment
-#virtualenv --no-site-packages -p python3 kivyinstall
-#. kivyinstall/bin/activate
-
-echo "[INFO]  Installing Cython(required when building kivy)..."
-pip3 install Cython==0.25.2
+# Install Cython
+pip install --upgrade pip
 pip install Cython==0.25.2
-echo "[DONE]  Cython Installed..."
-echo "[INFO]  Finally installing kivy(latest)..."
-pip3 install -U git+https://github.com/kivy/kivy.git@master
-echo "[DONE]  Installed Kivy..."
+
+# Install required Python libraries
+pip install kivy==1.11.1
+pip install kivy-garden
+pip install --upgrade requests
+pip install pymongo pandas
+pip install python3-xlib
+pip install numpy
+
+# Install matplotlib and graph via Kivy Garden
+chmod +x kivyenv/bin/garden
+kivyenv/bin/garden install matplotlib graph
+
+# Configure Kivy
+echo "[INFO] Configuring Kivy..."
+echo "from kivy.config import Config" >> kivyenv/lib/python3.5/site-packages/kivy/config.py
+echo "Config.set('graphics', 'window_system', 'sdl2')" >> kivyenv/lib/python3.5/site-packages/kivy/config.py
+echo "[DONE] Kivy Configuration Complete."
+
+# Enable X11 forwarding
+sudo sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
+sudo sed -i 's/#X11UseLocalhost yes/X11UseLocalhost no/' /etc/ssh/sshd_config
+sudo systemctl restart ssh
+
+# Replace the shebang in your garden executable script
+sed -i '1s|^.*$|#!/root/SilverStone-POS/kivyenv/bin/python3.5|' /root/.kivy/garden/garden.matplotlib/matplotlib/__init__.py
+
+# Run a simple X11 application to test, like xeyes
+xeyes
